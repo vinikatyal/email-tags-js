19dc1270e1fbccec921bf4629a3d179c151d875a
vini <vini@primaseller.com>
2015-12-15 23:31:39 +0530

    Js for creating email tags
    Create email tags and validate for email address

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(function(){
+  $('#tags input').on('focusout',function(){
+    //validate for email address
+    var regularExp = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
+    if(!regularExp.test(this.value)) {
+      alert("Invalid Email Address");
+      return;
+    }
+    var txt = this.value;
+    if(txt) {
+      $(this).before('<span class="tag">'+ txt.toLowerCase() +'</span>');
+    }
+    this.value="";
+  }).on('keyup',function( e ){
+    if(/(188|13)/.test(e.which)) $(this).focusout();
+
+  });
+
+  //remove tags
+  $('#tags').on('click','.tag',function(){
+     $(this).remove();
+  });
+
+});
